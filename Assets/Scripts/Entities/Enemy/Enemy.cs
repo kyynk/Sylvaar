27014225dedb44c02cb9c7f83211369c11bdc6df
garry <garry80633@gmail.com>
2024-12-22 00:15:20 +0800

    complete enemy

--- Assets/Scripts/Entities/Enemy/Enemy.cs
+++ Assets/Scripts/Entities/Enemy/Enemy.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+namespace Enemy
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Enemy : MonoBehaviour
     {
-        
-    }
+        public float Health { get; private set; } = 50f;
+        public float MaxHealth { get; private set; } = 50f;
+        public float Damage { get; private set; } = 10f;
+        public bool IsAlive => Health > 0;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void TakeDamage(float damage)
+        {
+            if (!IsAlive) return;
+
+            Health -= damage;
+            Health = Mathf.Clamp(Health, 0, MaxHealth);
+
+            if (!IsAlive)
+            {
+                Die();
+            }
+        }
+
+        public void Heal(float amount)
+        {
+            if (!IsAlive) return;
+
+            Health += amount;
+            Health = Mathf.Clamp(Health, 0, MaxHealth);
+        }
+
+        private void Die()
+        {
+            Debug.Log($"{gameObject.name} has died!");
+            // Optional: Disable or destroy the enemy object
+            Destroy(gameObject);
+        }
     }
 }
