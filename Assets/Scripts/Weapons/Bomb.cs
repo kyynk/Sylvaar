32e77312e0b0f746fcb748510c64d6958664822f
CuChaos <t110590048@ntut.org.tw>
2024-12-24 21:27:40 +0800

    bomb throw doing

--- Assets/Scripts/Weapons/Bomb.cs
+++ Assets/Scripts/Weapons/Bomb.cs
@@ -11,6 +11,10 @@ namespace Weapons
         public float Range => 5f;
         public float CooldownTime => 2f;
 
+        public float ThrowForce = 10f;
+        public float ThrowAngle = 45f;
+        public float AfterThrowDestroyTime = 2.1f;
+
         private float lastAttackTime;
 
         public void Attack()
@@ -30,8 +34,33 @@ namespace Weapons
 
         private void ThrowBomb()
         {
+            // create bomb
+            GameObject bombInstance = Instantiate(gameObject, transform.position, Quaternion.identity);
+
+            // delete coiped bomb script, avoid reapet excute Attack
+            Destroy(bombInstance.GetComponent<Bomb>());
+
+            // add rigibody by bomb
+            Rigidbody bombRigidbody = bombInstance.AddComponent<Rigidbody>();
+
+            // calculate throw direction
+            Vector3 throwDirection = CalculateThrowDirection();
+            bombRigidbody.AddForce(throwDirection * ThrowForce, ForceMode.VelocityChange);
+
+            // past 2 second Bomb explode
+            Destroy(bombInstance, AfterThrowDestroyTime); // add a little time, make sure have been deleted after explode
             Invoke(nameof(Explode), 1f);
         }
+
+        private Vector3 CalculateThrowDirection()
+        {
+            // according current forwad and angle to calculate throw direction
+            Vector3 forward = transform.forward;
+            Vector3 upward = Vector3.up * Mathf.Tan(ThrowAngle * Mathf.Deg2Rad);
+
+            return (forward + upward).normalized;
+        }
+
 
         private void Explode()
         {
