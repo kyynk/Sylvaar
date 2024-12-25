21e3d29ea92047f7f31f77c88a895268d48ca7fd
garry911128 <garry80633@gmail.com>
2024-12-25 14:23:42 +0800

    del chinese comment

--- Assets/Scripts/KeyboardInput/KeyboardInputManager.cs
+++ Assets/Scripts/KeyboardInput/KeyboardInputManager.cs
@@ -38,19 +38,17 @@ namespace KeyboardInput
             evtDpadAxis?.Invoke(axis);
             // wait AVG
             // evtDpadAxis?.Invoke(isMute ? Vector2.zero : axis);
-            // 使用相機方向轉換軸向
+           
             if (cameraTransform != null)
             {
                 Vector3 forward = cameraTransform.forward;
                 Vector3 right = cameraTransform.right;
 
-                // 確保 Y 軸為零，避免影響角色垂直方向
                 forward.y = 0;
                 right.y = 0;
                 forward.Normalize();
                 right.Normalize();
 
-                // 本地軸轉換為世界軸
                 axis = (forward * axis.z + right * axis.x).normalized;
             }
 
